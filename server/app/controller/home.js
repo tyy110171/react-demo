9e62e1237c25690ecbecf176b5fa99e0fa632957
tianyingying <tianyingying1@niwodai.net>
2019-08-27 11:19:43 +0800

    fix(home.js)fix build error

--- server/app/controller/home.js
+++ server/app/controller/home.js
@@ -1,6 +1,5 @@
 'use strict';
 const { Controller } = require('egg');
-// const token = 'xoxp-725565328610-738973929255-727643636226-76155c0cf2c1ba9fac61257268bd30e9';
 const userIdReg = /<@(.+)>/;
 
 class HomeController extends Controller {
@@ -10,72 +9,71 @@ class HomeController extends Controller {
     }
 
     async slackMessage() {
+        const token = process.env.SLACK_TOKEN;
         const { ctx } = this;
-        // const { text, user_name, channel_id } = ctx.request.body;
-        const { text} = ctx.request.body;
+        const { text, user_name, channel_id } = ctx.request.body;
         const mentionMatch = (text || '').match(userIdReg);
-        // let profileImg32 = '';
-        // let displayName = '';
+        let profileImg32 = '';
+        let displayName = '';
 
         if (mentionMatch) {
             const [ , mentionText ] = mentionMatch;
             const [ mentionedId ] = mentionText.split('|');
-            this.ctx.body = mentionedId;
-            // const profileResp = await this.ctx.curl(
-            //     `https://slack.com/api/users.profile.get?token=${token}&user=${mentionedId}`,
-            //     {
-            //         method: 'GET',
-            //         dataType: 'json',
-            //     }
-            // );
-            //
-            // this.ctx.messages = text;
-            //
-            // const profile = profileResp.data.profile;
-            //
-            // profileImg32 = profile.image_32;
-            // displayName = profile.display_name_normalized;
-            //
-            // await this.ctx.curl(
-            //     'https://slack.com/api/chat.postEphemeral',
-            //     {
-            //         method: 'POST',
-            //         dataType: 'json',
-            //         data: {
-            //             token,
-            //             channel: channel_id,
-            //             user: mentionedId,
-            //             text: 'Someone is mentioned you on APP praise',
-            //         },
-            //     }
-            // );
+            const profileResp = await this.ctx.curl(
+                `https://slack.com/api/users.profile.get?token=${token}&user=${mentionedId}`,
+                {
+                    method: 'GET',
+                    dataType: 'json',
+                }
+            );
+
+            this.ctx.messages = text;
+
+            const profile = profileResp.data.profile;
+
+            profileImg32 = profile.image_32;
+            displayName = profile.display_name_normalized;
+
+            await this.ctx.curl(
+                'https://slack.com/api/chat.postEphemeral',
+                {
+                    method: 'POST',
+                    dataType: 'json',
+                    data: {
+                        token,
+                        channel: channel_id,
+                        user: mentionedId,
+                        text: 'Someone is mentioned you on APP praise',
+                    },
+                }
+            );
         }
 
-        // ctx.body = {
-        //     response_type: 'in_channel',
-        //     blocks: [
-        //         {
-        //             type: 'section',
-        //             text: {
-        //                 type: 'mrkdwn',
-        //                 text: `*${user_name}*, Your praise is received!`,
-        //             },
-        //         },
-        //         {
-        //             type: 'section',
-        //             text: {
-        //                 type: 'mrkdwn',
-        //                 text: displayName,
-        //             },
-        //             accessory: {
-        //                 type: 'image',
-        //                 image_url: profileImg32,
-        //                 alt_text: 'avatar',
-        //             },
-        //         },
-        //     ],
-        // };
-        // ctx.set('Content-Type', 'application/json');
+        ctx.body = {
+            response_type: 'in_channel',
+            blocks: [
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: `*${user_name}*, Your praise is received!`,
+                    },
+                },
+                {
+                    type: 'section',
+                    text: {
+                        type: 'mrkdwn',
+                        text: displayName,
+                    },
+                    accessory: {
+                        type: 'image',
+                        image_url: profileImg32,
+                        alt_text: 'avatar',
+                    },
+                },
+            ],
+        };
+        ctx.set('Content-Type', 'application/json');
     }
 
     async message() {
